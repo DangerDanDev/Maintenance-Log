59c32060ad303847a2d5002d2fd76de69e296597
DangerDanDev <scyth3s@gmail.com>
2022-10-20 13:00:16 -0500

    Polished the RH panel of the log entry panel

--- src/LogEntryPanel.java
+++ src/LogEntryPanel.java
@@ -8,6 +8,10 @@ public class LogEntryPanel {
     private JCheckBox cbShowOnNotes;
     private JButton deleteButton;
     private JButton bSave;
+    private JButton bEdit;
+    private JComboBox cbShift;
+    private JTextArea textArea1;
+    private JTextField textField1;
 
     public JPanel getContentPanel() {
         return this.panel1;
@@ -15,10 +19,18 @@ public class LogEntryPanel {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setContentPane(new LogEntryPanel().getContentPanel());
-        frame.setSize(500,150);
+        LogEntryPanel entryPanel = new LogEntryPanel();
+        frame.setContentPane(entryPanel.getContentPanel());
+        frame.setSize(800,300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        String shifts[] = { "Mids", "Days", "Swings"};
+        cbShift = new JComboBox(shifts);
+        cbShift.setSelectedItem(shifts[1]);
     }
 }
 
