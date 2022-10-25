d51f7a38e5cc3021b55e7e67d123753b3609d7be
DangerDanDev <scyth3s@gmail.com>
2022-10-24 21:48:05 -0500

    Made a button on DiscrepancySnippet to open up a DiscrepancyViewer screen

--- src/GUI/DiscrepancyPanel.java
+++ src/GUI/DiscrepancyPanel.java
@@ -1,18 +1,42 @@
 package GUI;
 
+import data.Discrepancy;
+
 import javax.swing.*;
 
 public class DiscrepancyPanel {
     private JComboBox cbTail;
-    private JTextField textField1;
-    private JTextField textField2;
+    //private JTextField textField1;
+    //private JTextField textField2;
     private JTextArea tbTurnover;
     private JTextArea tbPartsOnOrder;
     private JComboBox cbStatus;
     private JPanel contentPane;
     private JTextArea tbNarrative;
 
+    private Discrepancy discrepancy;
+
     public JPanel getContentPane() {
         return this.contentPane;
     }
+
+    public DiscrepancyPanel(Discrepancy discrepancy) {
+        setDiscrepancy(discrepancy);
+    }
+
+    public Discrepancy getDiscrepancy() {
+        return discrepancy;
+    }
+
+    public void setDiscrepancy(Discrepancy discrepancy) {
+        this.discrepancy = discrepancy;
+
+        cbTail.setSelectedItem(discrepancy.getTailNum());
+        tbTurnover.setText(discrepancy.getTurnover());
+        tbPartsOnOrder.setText(discrepancy.getPartsOnOrder());
+        tbNarrative.setText(discrepancy.getNarrative());
+
+        //TODO: also set the status
+    }
+
 }
--- src/GUI/DiscrepancySnippet.java
+++ src/GUI/DiscrepancySnippet.java
@@ -3,17 +3,19 @@ package GUI;
 import data.Discrepancy;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class DiscrepancySnippet {
     private JTextArea tbNarrative;
     private JPanel panel1;
-    private JComboBox comboBox1;
-    private JCheckBox checkBox1;
+    private JButton btnViewDiscrepancy;
 
     private Discrepancy discrepancy;
 
     public DiscrepancySnippet(Discrepancy discrepancy) {
         setDiscrepancy(discrepancy);
+        btnViewDiscrepancy.addActionListener(new ViewDiscrepancyListener());
     }
 
     public Discrepancy getDiscrepancy() {
@@ -23,10 +25,25 @@ public class DiscrepancySnippet {
     public void setDiscrepancy(Discrepancy discrepancy) {
         this.discrepancy = discrepancy;
 
-        tbNarrative.setText(discrepancy.getNarrative());
+        if(discrepancy != null) {
+            tbNarrative.setText(discrepancy.getNarrative());
+        }
     }
 
     public JPanel getContentPane() {
         return panel1;
+    }
+
+    private class ViewDiscrepancyListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JFrame frame = new JFrame();
+            frame.setSize(800,600);
+
+            DiscrepancyPanel discrepancyPanel = new DiscrepancyPanel(getDiscrepancy());
+            frame.setContentPane(discrepancyPanel.getContentPane());
+
+            frame.setVisible(true);
+        }
     }
 }
