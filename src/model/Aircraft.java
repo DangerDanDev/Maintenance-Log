8e89078475ad9084db6e4d255511bc2f78598eb7
DangerDanDev <scyth3s@gmail.com>
2023-02-14 23:13:08 -0600

    Added an Aircraft class with tail number and an enabled flag that designates whether or not the aircraft will be shown on the notes/status board.

---
+++ src/model/Aircraft.java
@@ -0,0 +1,42 @@
+package model;
+
+import data.DatabaseObject;
+
+import java.time.Instant;
+
+public class Aircraft extends DatabaseObject {
+    private String tailNumber = "";
+
+    /**
+     * Whether or not this tail number is to be
+     * shown on the Notes and StatusBoard pages
+     */
+    private boolean enabled = true;
+
+    public Aircraft() {
+        super();
+    }
+
+    public Aircraft(long id, Instant dateCreated, Instant dateLastEdited, String tailNum, boolean enabled) {
+        super(id, dateCreated, dateLastEdited);
+
+        setTailNumber(tailNum);
+        setEnabled(enabled);
+    }
+
+    public String getTailNumber() {
+        return tailNumber;
+    }
+
+    public void setTailNumber(String tailNumber) {
+        this.tailNumber = tailNumber;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
