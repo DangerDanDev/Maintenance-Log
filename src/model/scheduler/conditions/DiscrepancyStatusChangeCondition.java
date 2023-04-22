13e3727f756dbed19aee54a548b2071c357fcb7b
DangerDanDev <scyth3s@gmail.com>
2023-04-22 16:16:40 -0500

    Made a functioning unit test for DiscrepancyStatusChangeCondition, it can now verify

--- src/model/scheduler/conditions/DiscrepancyStatusChangeCondition.java
+++ src/model/scheduler/conditions/DiscrepancyStatusChangeCondition.java
@@ -1,10 +1,12 @@
 package model.scheduler.conditions;
 
+import data.DBManager;
 import data.DatabaseObject;
 import data.queries.*;
 import data.tables.DiscrepancyTable;
 import data.tables.StatusTable;
 import data.tables.Table;
+import model.Discrepancy;
 import model.Status;
 import model.scheduler.Condition;
 
@@ -33,6 +35,10 @@ public class DiscrepancyStatusChangeCondition extends Condition {
         this.statusId = statusId;
     }
 
+    public DiscrepancyStatusChangeCondition(Discrepancy d, Status s) {
+        this(d.getId(), s.getId());
+    }
+
     @Override
     public boolean isMet() throws SQLException {
         return checkStatus();
@@ -41,14 +47,26 @@ public class DiscrepancyStatusChangeCondition extends Condition {
     private boolean checkStatus() throws SQLException  {
         Query discrepancyStatusQuery = new Query(DISCREPANCY_TABLE);
 
-        Criterion discrepancyIdMatches = new Criterion(DISCREPANCY_TABLE.COL_ID, "" + discrepancyId);
-        discrepancyStatusQuery.addWhereCriterion(discrepancyIdMatches, AndOr.AND);
+        Criterion my_discrepancy = new Criterion(DISCREPANCY_TABLE.COL_ID, "" + discrepancyId);
+        discrepancyStatusQuery.addWhereCriterion(my_discrepancy, AndOr.AND);
 
         Criterion statusIdMatches = new Criterion(STATUS_TABLE.COL_ID, "" + statusId);
         discrepancyStatusQuery.addWhereCriterion(statusIdMatches, AndOr.NONE);
 
-        discrepancyStatusQuery.addJoinClause(new JoinClause(JoinType.InnerJoin, STATUS_TABLE, DISCREPANCY_TABLE.COL_STATUS_ID, STATUS_TABLE.COL_ID));
+        discrepancyStatusQuery.addJoinClause(
+                new JoinClause(JoinType.InnerJoin, STATUS_TABLE, DISCREPANCY_TABLE.COL_STATUS_ID, STATUS_TABLE.COL_ID));
 
         return DISCREPANCY_TABLE.query(discrepancyStatusQuery).size() > 0;
+    }
+
+    public static void main(String[] args) throws SQLException {
+
+        DBManager.initialize();
+
+        Discrepancy d = DISCREPANCY_TABLE.getAllItems().get(0);
+
+        System.out.println(d.getStatus().getTitle());
+        DiscrepancyStatusChangeCondition condition = new DiscrepancyStatusChangeCondition(d.getId(), 1);
+        System.out.println("Condition is met: " + condition.isMet());
     }
 }
