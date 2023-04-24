2ff99890ba435ca78904affe1024249ed50e34ce
DangerDanDev <scyth3s@gmail.com>
2023-04-24 12:19:26 -0500

    Moves the NotesPanel logic from AppFrame to its own NotesPanel class

--- src/GUI/AppFrame.java
+++ src/GUI/AppFrame.java
@@ -18,11 +18,12 @@ public class AppFrame extends JFrame implements Refreshable {
     private JPanel notesPanel;
     private JScrollPane notesScrollPane;
 
+    private NotesPanel notesPanelMngr;
 
     public AppFrame() throws SQLException {
         super("Hawk Logbook");
 
-        loadNotes();
+        refresh();
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setJMenuBar(new MenuManager().menuBar);
@@ -33,41 +34,28 @@ public class AppFrame extends JFrame implements Refreshable {
 
         Scheduler.init();
     }
-    private HashMap<Aircraft, AircraftHeader> aircraftHeaders = new HashMap<>();
-    /**
-     * Loads all the relevant discrepancies and populates the NotesPanel with them
-     * @throws SQLException
-     */
-    private void loadNotes() {
-        try {
-
-            notesPanel.removeAll();
-            aircraftHeaders.clear();
-
-            notesPanel.setLayout(new BoxLayout(notesPanel, BoxLayout.Y_AXIS));
-
-            for (Aircraft aircraft : AircraftTable.getInstance().getAllItems()) {
-                AircraftHeader header = new AircraftHeader(this, null, aircraft);
-                aircraftHeaders.put(aircraft, header);
-                notesPanel.add(header.getContentPane());
-            }
-
-
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(this, "There was an error loading aircraft from the table.");
-            System.err.println(ex.getMessage());
-        }
-    }
 
     public void refresh() {
-        loadNotes();
 
         try {
+
+            notesPanelMngr.refresh();
+
             Scheduler.getInstance().checkTriggers();
+
+            revalidate();
+            repaint();
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "The scheduler encountered an SQL error");
             System.err.println(ex.getMessage());
         }
+    }
+
+    private void createUIComponents() {
+
+        //The NotesPanel has a good bit of custom initiation code
+        notesPanelMngr = new NotesPanel(this);
+        notesPanel = notesPanelMngr;
     }
 
 
---
+++ src/GUI/NotesPanel.java
@@ -0,0 +1,43 @@
+package GUI;
+
+import data.queries.AndOr;
+import data.queries.Criterion;
+import data.queries.Query;
+import data.queries.WhereClause;
+import data.tables.AircraftTable;
+import data.tables.Table;
+import model.Aircraft;
+
+import javax.swing.*;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class NotesPanel extends JPanel {
+
+    private AircraftTable aircraftTable = AircraftTable.getInstance();
+
+    private HashMap<Aircraft, AircraftHeader> aircraftHeaders = new HashMap<>();
+
+    private JFrame appFrame;
+
+    public NotesPanel(JFrame appFrame) {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+    }
+
+    public void refresh() throws SQLException {
+
+            removeAll();
+            aircraftHeaders.clear();
+
+            for (Aircraft aircraft : aircraftTable.getAllItems())
+                addAircraft(aircraft);
+
+    }
+
+    private void addAircraft(Aircraft aircraft) {
+        AircraftHeader header = new AircraftHeader(appFrame, null, aircraft);
+        aircraftHeaders.put(aircraft, header);
+        this.add(header.getContentPane());
+    }
+}
